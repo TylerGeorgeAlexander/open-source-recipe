23c4bafb41bd9dd895dc241a6b3d060e8c1ac769
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-10-16 14:36:07 -0700

    notes and TODO tree stuff done

---
+++ notes.md
@@ -0,0 +1,3 @@
+npx tailwindcss -i ./public/css/style.css -o ./public/css/output.css --watch
+
+set up some sort of script for the above command
--- routes/recipes.js
+++ routes/recipes.js
@@ -9,6 +9,9 @@ router.get("/:id", ensureAuth, recipesController.getRecipe);
 
 router.post("/createRecipe", upload.single("file"), recipesController.createRecipe);
 
+// TODO
+// router.post("/favoriteRecipe/:id", recipesController.favoriteRecipe);
+
 router.put("/likeRecipe/:id", recipesController.likeRecipe);
 
 router.delete("/deleteRecipe/:id", recipesController.deleteRecipe);
